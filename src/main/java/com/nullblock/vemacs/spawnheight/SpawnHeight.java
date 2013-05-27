c3f042f8aa62112fe936112c69f311afe8543f33
vemacs <gentoo@outlook.com>
2013-05-27 17:28:16 -0600

    Initial commit

---
+++ src/main/java/com/nullblock/vemacs/spawnheight/SpawnHeight.java
@@ -0,0 +1,29 @@
+package com.nullblock.vemacs.spawnheight;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.CreatureSpawnEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SpawnHeight extends JavaPlugin implements Listener {
+	public void onDisable() {
+	}
+
+	public void onEnable() {
+		getServer().getPluginManager().registerEvents(this, this);
+		this.saveDefaultConfig();
+		this.getLogger().info(
+				"Starting SpawnHeight with max "
+						+ this.getConfig().getInt("max") + " and min "
+						+ this.getConfig().getInt("min"));
+	}
+
+	@EventHandler(priority = EventPriority.LOW)
+	public void onMobSpawn(CreatureSpawnEvent event) {
+		if (event.getLocation().getY() <= this.getConfig().getInt("max")
+				|| event.getLocation().getY() >= this.getConfig().getInt("min")) {
+			event.setCancelled(true);
+		}
+	}
+}
