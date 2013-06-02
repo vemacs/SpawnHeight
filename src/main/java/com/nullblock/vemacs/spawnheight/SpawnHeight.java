4785161c3a9ea94d1cea4273934feab31dfc82f7
vemacs <gentoo@outlook.com>
2013-06-02 12:05:20 -0600

    Add probability

--- src/main/java/com/nullblock/vemacs/spawnheight/SpawnHeight.java
+++ src/main/java/com/nullblock/vemacs/spawnheight/SpawnHeight.java
@@ -8,6 +8,7 @@ import org.bukkit.event.entity.CreatureSpawnEvent.SpawnReason;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class SpawnHeight extends JavaPlugin implements Listener {
+
 	public void onDisable() {
 	}
 
@@ -16,18 +17,29 @@ public class SpawnHeight extends JavaPlugin implements Listener {
 		this.saveDefaultConfig();
 		this.getLogger().info(
 				"Starting SpawnHeight with max "
-						+ this.getConfig().getInt("max") + " and min "
-						+ this.getConfig().getInt("min"));
+						+ this.getConfig().getInt("max") + ", min "
+						+ this.getConfig().getInt("min") + " and probability "
+						+ Float.parseFloat(this.getConfig().getString("prob")));
 	}
 
 	@EventHandler(priority = EventPriority.HIGHEST)
 	public void onMobSpawn(CreatureSpawnEvent event) {
-
-		if (((int) event.getLocation().getY() >  this.getConfig().getInt("max") || (int) event
+		if (((int) event.getLocation().getY() > this.getConfig().getInt("max") || (int) event
 				.getLocation().getY() < this.getConfig().getInt("min"))
 				&& event.getSpawnReason().equals(SpawnReason.NATURAL)) {
-			this.getLogger().info("Spawn prevented at " + event.getLocation().getY());
-			event.setCancelled(true);
+			float f = Float.parseFloat(this.getConfig().getString("prob"));
+			if (!canSpawn(f)) {
+				event.setCancelled(true);
+			}
 		}
 	}
+
+	public boolean canSpawn(float f) {
+		float compare = (float) Math.random();
+		if (compare <= f) {
+			return true;
+		}
+		return false;
+	}
+
 }
